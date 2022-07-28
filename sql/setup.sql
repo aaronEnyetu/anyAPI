b249fb3d9b473210537c6bcfb07af1282c2cdb40
Aaron Enyetu <aaronenyetu@gmail.com>
2022-07-28 11:39:38 -0700

    Database setup complete

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,18 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP table if exists game_consoles;
+
+
+CREATE table game_consoles (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR NOT NULL,
+    description VARCHAR NOT NULL,
+    year INT NOT NULL
+);
+
+INSERT INTO game_consoles (name, description, year) VALUES
+('Atari', 'Classically viewed as the original game console.', 1977),
+('NES', 'Considered to be what put gaming consoles onto the market.', 1983),
+('Sega', 'Nintendos competitor. Failed to compete on the first wave of consoles.', 1983),
+('Sony Playstation', 'The game changer. This console set a new standard for gaming. 3D models and environments were starting to become the new standard. It led its generation', 1994),
+('Nintendo 64', 'This competed with the PS1 for the most part and was overall loved despite losing out in the end.', 1996);
