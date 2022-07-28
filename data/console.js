792a7c87e6a65dde6d9acb69c27819f00145748d
Aaron Enyetu <aaronenyetu@gmail.com>
2022-07-28 14:34:13 -0700

    created the data file

---
+++ data/console.js
@@ -0,0 +1,37 @@
+const consoles = [
+  {
+    id: '1',
+    name: 'Atari',
+    description: 'Classically viewed as the original game console.',
+    year: 1977,
+  },
+  {
+    id: '2',
+    name: 'NES',
+    description: 'Considered to be what put gaming consoles onto the market.',
+    year: 1983,
+  },
+  {
+    id: '3',
+    name: 'Sega',
+    description:
+        'Nintendos competitor. Failed to compete on the first wave of consoles.',
+    year: 1983,
+  },
+  {
+    id: '4',
+    name: 'Sony Playstation',
+    description:
+        'The game changer. This console set a new standard for gaming. 3D models and environments were starting to become the new standard. It led its generation',
+    year: 1994,
+  },
+  {
+    id: '5',
+    name: 'Nintendo 64',
+    description:
+        'This competed with the PS1 for the most part and was overall loved despite losing out in the end.',
+    year: 1996,
+  },
+];
+  
+module.exports = { consoles };
